073b3b346170336c7385922498e2cd699a379764
Sylvain Emery <sylvain@1000mercis.com>
2015-02-27 20:32:24 +0100

    1st commit

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-directory structure:
+Polymexp
+--------
 
-```
-/app
-  /elements
-    my-element-1/ -> contains all the files of your polymer element (css, html, js...)
-    elements.html -> contains all the imports (this is *this* file that will be vulcanized). e.g:
-      <link rel="import" href="../../bower_components/core-item/core-item.html">
-      <link rel="import" href="yo-greeting/yo-greeting.html">
-  /images -> will be imagemin'ed
-  /scripts
-    app.js -> contains your main polymer app code
-  /styles
-  /test -> put your tests here (web-component-tester)
-  index.html
-```
+My first Polymer experiment
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,74 @@
 (function (document) {
   'use strict';
 
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
-  app.appName = 'Yo, Polymer App!';
+  var DEFAULT_ROUTE = "home";
+  var globals;
 
-  // Listen for template bound event to know when bindings
-  // have resolved and content has been stamped to the page
-  app.addEventListener('template-bound', function() {
-    console.log('Our app is ready to rock!');
+  app.addEventListener('template-bound', function(e) {
+    // Use URL hash for initial route. Otherwise, use the first page.
+    this.route = this.route || DEFAULT_ROUTE;
+
+    app.$.additemform.addEventListener('additem', function(e) {
+      console.log(e.type, e.detail);
+      app.onAddItem(e.detail);
+    });
+    globals = app.$.globals;
   });
+
+  app.menuItemSelected = function(e, detail, sender) {
+    if (detail.isSelected) {
+      document.querySelector('#scaffold').closeDrawer();
+    }
+  };
+
+  app.onAddItem = function(item) {
+    // find the date first
+    for (var i = 0; i < globals.dateArray.length && globals.dateArray[i].dateValue.valueOf() < item.date.valueOf(); i++) {}
+      if (i < globals.dateArray.length && globals.dateArray[i].dateValue.valueOf() != item.date.valueOf()) {
+        // add a new date somewhere in the middle of the array
+        globals.dateArray.splice(i, 0, {
+          dateValue: item.date,
+          label: item.date.toDateString(),
+          dayList: new Array()
+        });
+      }
+      else if (i >= globals.dateArray.length) {
+        //add a new date at the end of the array
+        globals.dateArray.push({
+          dateValue: item.date,
+          label: item.date.toDateString(),
+          dayList: new Array()
+        });
+      }
+
+      //add the new item
+      for (var j = 0; j < globals.dateArray[i].dayList.length && globals.dateArray[i].dayList[j].timeOfDay.value < item.timeOfDay.value; j++) {}
+        if (j < globals.dateArray[i].dayList.length && globals.dateArray[i].dayList[j].timeOfDay.value != item.timeOfDay.value) {
+          // add a new timeofday somewhere in the middle of the array
+          globals.dateArray[i].dayList.splice(j, 0, {
+            timeOfDay: item.timeOfDay,
+            items: new Array()
+          });
+        }
+        else if (j >= globals.dateArray[i].dayList.length) {
+          //add a new timeofday at the end of the array
+          globals.dateArray[i].dayList.push({
+            timeOfDay: item.timeOfDay,
+            items: new Array()
+          });
+        }
+        //add a new item
+        globals.dateArray[i].dayList[j].items.push({
+          name: item.name
+        });
+
+        console.log('item added');
+        this.route = "list";
+      };
+
+
+
 
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
